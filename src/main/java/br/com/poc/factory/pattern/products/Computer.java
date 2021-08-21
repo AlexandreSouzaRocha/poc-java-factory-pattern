06ebe98cae550d1a53bfbb2be415ea19fe0dc61c
Alexandre Souza Rocha <alexandresouzarocha943@gmail.com>
2021-08-21 14:47:15 -0300

    Refactoring package from partners to products and adding new class Computer

--- src/main/java/br/com/poc/factory/pattern/products/Computer.java
+++ src/main/java/br/com/poc/factory/pattern/products/Computer.java
@@ -1,7 +1,7 @@
-package br.com.poc.factory.pattern.partners;
+package br.com.poc.factory.pattern.products;
 
-import br.com.poc.factory.pattern.factories.CnabFactory;
-import br.com.poc.factory.pattern.interfaces.ICnab;
+import br.com.poc.factory.pattern.factories.ProductFactory;
+import br.com.poc.factory.pattern.interfaces.IProduct;
 
-public class Bradesco extends CnabFactory implements ICnab {
+public class Computer extends ProductFactory implements IProduct {
 }
