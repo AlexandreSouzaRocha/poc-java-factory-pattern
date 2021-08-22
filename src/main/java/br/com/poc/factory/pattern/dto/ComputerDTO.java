d51dec2b6bd1db7aa97ea6965d7220febe099f14
Alexandre Souza Rocha <alexandresouzarocha943@gmail.com>
2021-08-22 14:16:00 -0300

    Implementing toString method

---
+++ src/main/java/br/com/poc/factory/pattern/dto/ComputerDTO.java
@@ -0,0 +1,47 @@
+package br.com.poc.factory.pattern.dto;
+
+import com.google.gson.Gson;
+
+public class ComputerDTO {
+    private String name;
+    private int ramSize;
+    private int hardDiskSize;
+    private String processorName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRamSize() {
+        return ramSize;
+    }
+
+    public void setRamSize(int ramSize) {
+        this.ramSize = ramSize;
+    }
+
+    public int getHardDisSize() {
+        return hardDiskSize;
+    }
+
+    public void setHardDisSize(int hardDiskSize) {
+        this.hardDiskSize = hardDiskSize;
+    }
+
+    public String getProcessorName() {
+        return processorName;
+    }
+
+    public void setProcessorName(String processorName) {
+        this.processorName = processorName;
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+}
