bb83f89530baf755823c0b2285344656e97627aa
Alexandre Souza Rocha <alexandresouzarocha943@gmail.com>
2021-08-22 14:17:31 -0300

    Implementing base interface for each product

--- src/main/java/br/com/poc/factory/pattern/products/Computer.java
+++ src/main/java/br/com/poc/factory/pattern/products/Computer.java
@@ -1,7 +1,33 @@
 package br.com.poc.factory.pattern.products;
 
-import br.com.poc.factory.pattern.factories.ProductFactory;
+import br.com.poc.factory.pattern.dto.ComputerDTO;
 import br.com.poc.factory.pattern.interfaces.IProduct;
 
-public class Computer extends ProductFactory implements IProduct {
+import javax.swing.*;
+
+public class Computer implements IProduct {
+    private ComputerDTO computerDTO;
+
+    public Computer() {
+        this.computerDTO = new ComputerDTO();
+    }
+
+    @Override
+    public void sellProduct() {
+        JOptionPane.showMessageDialog(null, "Product Sold: " + this.getProductDetails(), "Factory Pattern", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public <T> T getProductDetails() {
+        this.setComputer();
+        return (T) this.computerDTO.toString();
+    }
+
+    public void setComputer() {
+        this.computerDTO.setName("DELL");
+        this.computerDTO.setRamSize(16000);
+        this.computerDTO.setProcessorName("INTEL");
+        this.computerDTO.setHardDisSize(2048);
+    }
 }
+
