a7d891983b832449b94484f3325ce748a95a8c17
Alexandre Souza Rocha <alexandresouzarocha943@gmail.com>
2021-08-22 14:14:14 -0300

    Implementing factory creating switching product type

--- src/main/java/br/com/poc/factory/pattern/factory/ProductFactory.java
+++ src/main/java/br/com/poc/factory/pattern/factory/ProductFactory.java
@@ -1,4 +1,23 @@
-package br.com.poc.factory.pattern.factories;
+package br.com.poc.factory.pattern.factory;
+
+
+import br.com.poc.factory.pattern.constants.ProductConstants;
+import br.com.poc.factory.pattern.creators.ComputerCreator;
+import br.com.poc.factory.pattern.creators.HeadPhoneCreator;
+import br.com.poc.factory.pattern.creators.MonitorCreator;
 
 public class ProductFactory {
+    public Product makeProduct(String type) throws Exception {
+        switch (type) {
+            case ProductConstants.COMPUTER:
+                return new ComputerCreator();
+            case ProductConstants.HEADPHONE:
+                return new HeadPhoneCreator();
+            case ProductConstants.MONITOR:
+                return new MonitorCreator();
+            default:
+                throw new Exception("The product type is not valid.");
+        }
+    }
+
 }
