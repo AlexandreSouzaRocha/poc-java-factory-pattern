980abf7b0c1a984ff712a7193fd3e154cf2e3940
Alexandre Souza Rocha <alexandresouzarocha943@gmail.com>
2021-08-22 14:17:13 -0300

    Implementing base interface for each product

--- src/main/java/br/com/poc/factory/pattern/products/Monitor.java
+++ src/main/java/br/com/poc/factory/pattern/products/Monitor.java
@@ -1,4 +1,32 @@
 package br.com.poc.factory.pattern.products;
 
-public class Monitor {
+import br.com.poc.factory.pattern.dto.MonitorDTO;
+import br.com.poc.factory.pattern.interfaces.IProduct;
+
+import javax.swing.*;
+
+public class Monitor implements IProduct {
+    private MonitorDTO monitorDTO;
+
+    public Monitor() {
+        this.monitorDTO = new MonitorDTO();
+    }
+
+    @Override
+    public void sellProduct() {
+        JOptionPane.showMessageDialog(null, "Product Sold: " + this.getProductDetails(), "Factory Pattern", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public <T> T getProductDetails() {
+        this.setMonitor();
+        return (T) this.monitorDTO.toString();
+    }
+
+    public void setMonitor() {
+        this.monitorDTO.setName("DELL XPS 17");
+        this.monitorDTO.setBrand("DELL");
+        this.monitorDTO.setHertzValue(144);
+        this.monitorDTO.setInches(32);
+    }
 }
