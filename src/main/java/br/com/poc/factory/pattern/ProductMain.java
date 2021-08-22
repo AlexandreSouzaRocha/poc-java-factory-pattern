8ac30ab4cf14de3fb3dbb6c810a620d4bb974e36
Alexandre Souza Rocha <alexandresouzarocha943@gmail.com>
2021-08-22 14:19:43 -0300

    Implementing example to show factory method funcionallity

---
+++ src/main/java/br/com/poc/factory/pattern/ProductMain.java
@@ -0,0 +1,20 @@
+package br.com.poc.factory.pattern;
+
+import br.com.poc.factory.pattern.factory.Product;
+import br.com.poc.factory.pattern.factory.ProductFactory;
+
+public class ProductMain {
+    private static Product product;
+    private static ProductFactory productFactory;
+
+    public static void main(String[] args) {
+        productFactory = new ProductFactory();
+        try {
+            product = productFactory.makeProduct("MONITOR");
+            product.sellProduct();
+        } catch (Exception e) {
+            System.out.println("Unhandled error: " + e.getMessage());
+            System.exit(0);
+        }
+    }
+}
