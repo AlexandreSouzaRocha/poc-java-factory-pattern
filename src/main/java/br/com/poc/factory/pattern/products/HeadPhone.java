0051c8081a36095420e1a08bc5093fcbdf30962e
Alexandre Souza Rocha <alexandresouzarocha943@gmail.com>
2021-08-22 14:17:40 -0300

    Implementing base interface for each product

--- src/main/java/br/com/poc/factory/pattern/products/HeadPhone.java
+++ src/main/java/br/com/poc/factory/pattern/products/HeadPhone.java
@@ -1,4 +1,34 @@
 package br.com.poc.factory.pattern.products;
 
-public class HeadPhone {
+import br.com.poc.factory.pattern.dto.HeadPhoneDTO;
+import br.com.poc.factory.pattern.factory.Product;
+import br.com.poc.factory.pattern.interfaces.IProduct;
+
+import javax.swing.*;
+
+public class HeadPhone implements IProduct {
+    private HeadPhoneDTO headPhoneDTO;
+
+    public HeadPhone() {
+        this.headPhoneDTO = new HeadPhoneDTO();
+    }
+
+    @Override
+    public void sellProduct() {
+        JOptionPane.showMessageDialog(null, "Product Sold: " + this.getProductDetails(), "Factory Pattern", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public <T> T getProductDetails() {
+        this.setHeadPhone();
+        return (T) this.headPhoneDTO.toString();
+    }
+
+    public void setHeadPhone() {
+        this.headPhoneDTO.setName("LOGITECH G PRO");
+        this.headPhoneDTO.setBrandName("LOGITECH");
+        this.headPhoneDTO.setChannel("7.1");
+        this.headPhoneDTO.setMegaHertz(1777);
+        this.headPhoneDTO.setHasMicrophone(true);
+    }
 }
