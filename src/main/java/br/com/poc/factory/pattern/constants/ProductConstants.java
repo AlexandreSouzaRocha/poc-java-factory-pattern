5985f90c73d95742ac18ca264126cbf9ad8566b1
Alexandre Souza Rocha <alexandresouzarocha943@gmail.com>
2021-08-22 14:14:33 -0300

    Implementing constants for each product type

---
+++ src/main/java/br/com/poc/factory/pattern/constants/ProductConstants.java
@@ -0,0 +1,7 @@
+package br.com.poc.factory.pattern.constants;
+
+public class ProductConstants {
+    public static final String COMPUTER = "COMPUTER";
+    public static final  String HEADPHONE = "HEADPHONE";
+    public static final String MONITOR = "MONITOR";
+}
